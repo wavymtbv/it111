45e39bd4d1d632819a366967d36d4289583a3260
wavymtbv <165719562+wavymtbv@users.noreply.github.com>
2024-04-18 14:24:19 -0700

    Add files via upload

---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,9 @@
+public class MyVariable1 {
+
+    public static void main(String[] args) {
+String name = "Maggie";
+    int hours = 48;
+    System.out.println(name);
+    System.out.println(hours);
+    }
+}
