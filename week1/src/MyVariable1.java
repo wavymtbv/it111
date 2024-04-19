f29aed9bcf7e037205a877f8382f554edb0cdbcb
wavymtbv <165719562+wavymtbv@users.noreply.github.com>
2024-04-19 09:21:42 -0700

    Delete week1 directory

--- week1/src/MyVariable1.java
+++
@@ -1,9 +0,0 @@
-public class MyVariable1 {
-
-    public static void main(String[] args) {
-String name = "Maggie";
-    int hours = 48;
-    System.out.println(name);
-    System.out.println(hours);
-    }
-}
